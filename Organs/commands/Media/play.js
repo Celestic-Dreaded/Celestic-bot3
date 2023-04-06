4f9490078e9e18d060d0e79ab4e153186f9b4252
Nimesh Official <89264186+nimesh-official@users.noreply.github.com>
2023-04-07 01:32:45 +0530

    Update play.js

--- Organs/commands/Media/play.js
+++ Organs/commands/Media/play.js
@@ -1,7 +1,6 @@
-const YT = require('../../../lib/ytdl')
+const axios = require('axios')
 const { isUrl, fetchBuffer } = require('../../../lib/Function')
 const fs=require("fs")
-const yts= require("yt-search")
 require ('../../../settings')
 module.exports={
     name:"play",
@@ -13,7 +12,7 @@ module.exports={
     start:async(client,m,{command,prefix,text,args})=>{
                
 if (!text)  return m.reply(`Please Enter Youtube Song Name`)
-song = await fetchBuffer(`https://api.zeeoneofc.my.id/api/downloader/youtube-playmp3?apikey=Lja3LTBXKt53Gm4&query=${text}`)
+song = await axios.get(`https://api.zeeoneofc.my.id/api/downloader/youtube-playmp3?apikey=Lja3LTBXKt53Gm4&query=${text}`)
 await client.sendMessage(m.from,{audio: song.result.download,fileName: ${song.result.title}.mp3',mimetype: 'audio/mpeg'},{quoted:m})
     }
 }
