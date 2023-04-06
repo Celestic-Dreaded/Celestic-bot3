8859af88eb42ab3982af8235d3e9592d90237608
Nimesh Official <89264186+nimesh-official@users.noreply.github.com>
2023-04-07 00:46:57 +0530

    Update play.js

--- Organs/commands/Media/play.js
+++ Organs/commands/Media/play.js
@@ -13,7 +13,7 @@ module.exports={
     start:async(client,m,{command,prefix,text,args})=>{
                
 if(!text) return client.sendMessage(m.from,{text:"What you want to play"},{quoted:m})
-song = await fetchJson(`https://api.zeeoneofc.my.id/api/downloader/youtube-playmp3?apikey=Lja3LTBXKt53Gm4&query=${q}`)
+song = await fetchBuffer(`https://api.zeeoneofc.my.id/api/downloader/youtube-playmp3?apikey=Lja3LTBXKt53Gm4&query=${q}`)
 await client.sendMessage(m.from,{
     audio: { url: song.result.download },
     fileName: song.result.title + '.mp3',
